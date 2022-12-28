dd02fdac6e305369fbb42b79b4f12b1fb2b9e19d
Abhishek Tiwari <115910967+Abhi0412Shek@users.noreply.github.com>
2022-12-28 22:37:42 +0530

    Add files via upload

---
+++ class1-EscpeChar-cipherSchool.py
@@ -0,0 +1,10 @@
+#output : Line A \n Line B
+print("Line A \\n Line B")
+print("Line B \\t line b")
+print("this is double backslash \\\\")
+print("this is 4 backslash \\\\\\\\")
+#output : "\" \'
+print("\\\" \\\' ")
+#\'-'(b)
+#\\- \ (a)
+#\\\' - \'
