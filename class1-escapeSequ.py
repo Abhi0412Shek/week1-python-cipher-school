62a1a6b01495f83c1b4c7a0329d82bceec58863f
Abhishek Tiwari <115910967+Abhi0412Shek@users.noreply.github.com>
2022-12-29 10:16:40 +0530

    Add files via upload

---
+++ class1-escapeSequ.py
@@ -0,0 +1,12 @@
+print("hello \"world\" world")
+print('I\'m rahul')
+#backslash n 
+print("line A\nline B\nline C")
+#backslash t
+print("Name\tRahul")
+#backslash
+print("This is backslash\\")
+#how to use double backslash 
+print("this is blackslash\\\\")
+#backslash b
+print("hell\blo")
