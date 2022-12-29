77d69307ec1d5a239e46ec4a7025f1d2482ace86
Abhishek Tiwari <115910967+Abhi0412Shek@users.noreply.github.com>
2022-12-29 10:10:04 +0530

    Add files via upload

---
+++ class1-printfunction-cipherschool.py
@@ -0,0 +1,9 @@
+print("hello world")
+#print using single quote
+print('hello world')
+#using single quote in double quote
+print("hello 'world' world")
+#using double quote in single quote
+print('hello "world" world')
+
+print("'I'm Abhishek")
