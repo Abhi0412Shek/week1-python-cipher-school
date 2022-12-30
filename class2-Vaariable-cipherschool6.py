770699b98d7c05dbd43cffb0fc561d32edf87bc4
Abhishek Tiwari <115910967+Abhi0412Shek@users.noreply.github.com>
2022-12-30 22:55:46 +0530

    Add files via upload

---
+++ class2-Vaariable-cipherschool6.py
@@ -0,0 +1,18 @@
+number1=2
+print(number1)
+number1=4
+print(number1)
+#string,number
+name="Abhishek"
+print(name)
+name=123
+print(name)
+
+
+#rule 1
+#you canot start your variable name by a number
+#you can start your variable name by letter,underscore
+#_name="Rahul"
+
+user_one_name ="Inderjit"  #snake case writing
+userOneName="Ram"  #camel case writing
