9d4d39198f38755082a1eb72d8f6761712c11f59
Abhishek Tiwari <115910967+Abhi0412Shek@users.noreply.github.com>
2022-12-30 22:55:17 +0530

    Add files via upload

---
+++ class2-strConcat-cipherSchool4.py
@@ -0,0 +1,10 @@
+#collection of character inside single quotes or double quotes
+
+first_name = "Abhishek"
+last_name = "Tiwari"
+full_name = first_name + "  " + last_name
+print(full_name)
+#print(first_name+3).....error
+#print(first_name +"3")....no error
+#print(first_name + str(3))...no error
+print(full_name * 3)
